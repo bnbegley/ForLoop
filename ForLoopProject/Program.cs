1a492c88ad86b211919b01b4720602c43884d28a
brail33na11@gmail.com <brail33na11@gmail.com>
2019-07-26 10:05:48 -0400

    intial commit

--- ForLoopProject/Program.cs
+++ ForLoopProject/Program.cs
@@ -3,7 +3,20 @@
 namespace ForLoopProject {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            int[] ints = { 859, 941, 223, 845,
+                    417, 488, 346, 700, 789, 628, 491,
+                    816, 538, 962, 931, 401, 566, 810,
+                    974, 122, 174, 485, 953, 335, 463
+            };
+            int total = 0;
+            for(int idx = 0; idx<ints.Length; idx++) {
+                total += ints[idx];
+            }
+
+            decimal average = total / ints.Length;
+            Console.WriteLine($"Total: {total}" + 
+                $" Average: {average}.");
+                
         }
     }
 }
